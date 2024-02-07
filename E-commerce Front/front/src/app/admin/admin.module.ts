32b2c4323f507d27f2c0bcc0d79c8bf80c111006
karen <karennn332@gmail.com>
2024-02-07 15:47:59 -0300

    bug add Category

--- E-commerce Front/front/src/app/admin/admin.module.ts
+++ E-commerce Front/front/src/app/admin/admin.module.ts
@@ -4,16 +4,37 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { PostCategoryComponent } from './components/post-category/post-category.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatError, MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostCategoryComponent
+   
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconButton,
+    MatSnackBarModule,
+    HttpClientModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class AdminModule { }
